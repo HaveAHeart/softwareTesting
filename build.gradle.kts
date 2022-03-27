cfc8b3ef6ed4e13de8fbc9762c8497fa6deb1a6e
Alexey <bedrin18@mail.ru>
2022-03-27 19:53:42 +0300

    testing coverage added

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
+    id("org.jetbrains.kotlinx.kover") version "0.5.0"
     application
 }
 
@@ -16,14 +17,19 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.koverMergedHtmlReport {
+    isEnabled = true                        // false to disable report generation
+    htmlReportDir.set(layout.projectDirectory.dir("results/coverage"))
 }
